6aa0ec726f47cae7963520f77ccbb04cef60e84c
andriy97 <andrii.diachuk13@gmail.com>
2022-03-27 17:51:50 +0200

    Initial commit

---
+++ app/Models/Flyers.php
@@ -0,0 +1,63 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Spatie\QueryBuilder\QueryBuilder;
+
+
+
+class Flyers extends Model
+{
+    use HasFactory;
+    
+    static function getFlyers(){
+        
+        $now=date('Y-m-d');
+        $limit=isset(request()->query()['limit']) ? (int)request()->query()['limit'] : 100; //default 100
+        $allowedFilters=array('is_published', 'category');
+        $notAllowedFilters=array_diff_key( request()->query()['filter'], array_flip($allowedFilters));
+    
+
+        $flyers = QueryBuilder::for(Flyers::class)
+        //default params
+        ->where('start_date', '<=', $now)
+        ->where('end_date', '>=', $now)
+        //filters
+        ->allowedFilters($allowedFilters)
+        ->paginate($limit)
+        ->appends(request()->query());
+
+        //response for not allowed filters
+        if(sizeOf($notAllowedFilters)>0)
+            return response()->json([
+                "success"=> false,
+                "code"=> 400,
+                "error"=> array(
+                    "message"=>"Bad Request",
+                    "debug"=>"Not allowed filters: ".implode(", ",array_keys($notAllowedFilters))
+                )
+            ], 200);;
+        
+        //successful response
+        $is_empty=$flyers->getCollection()->isEmpty();
+        if(!$is_empty)
+            return response()->json([
+                "success"=> true,
+                "code"=> 200,
+                "results"=> $flyers->getCollection()
+            ],200);
+        else//no matching
+            return response()->json([
+                "success"=> false,
+                "code"=> 404,
+                "error"=> array(
+                    "message"=>"Not Found",
+                    "debug"=>"No data found with these filters: ".http_build_query(request()->query()['filter'],'',', ')
+                )
+            ],200);
+    }
+
+
+}
---
+++ database/migrations/2022_03_26_143621_create_flyers_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('flyers', function (Blueprint $table) {
+            $table->integer('id');
+            $table->string('title');
+            $table->date('start_date');
+            $table->date('end_date');
+            $table->boolean('is_published');
+            $table->string('retailer');
+            $table->string('category');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('flyers');
+    }
+};
